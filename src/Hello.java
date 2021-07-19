8141af970b8798b52b2cb9871a6c0e9322f2ac0f
remusz2000 <REMUSZ2000@GMAIL.COM>
2021-07-19 15:20:34 -0500

    added one more print out statement

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git 2");
+        System.out.println("I have VCS menu instead of Git");
 
     }
 }
