2e493d8e01a42b015c2b0de09ea54ea30896851a
remusz2000 <REMUSZ2000@GMAIL.COM>
2021-07-19 15:42:15 -0500

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -3,5 +3,7 @@ public class GitCool {
     public static void main(String[] args) {
         // B23 is learning git
         // B23 is special number
+
+        //We have added github under intelliJ
     }
 }
